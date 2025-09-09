db18891f7eeaf8593554a2b18da017e217137a40
riazbd <nuruddinriaz5@gmail.com>
2025-09-09 15:15:30 +0600

    init for prod

---
+++ sanity.config.ts
@@ -0,0 +1,37 @@
+'use client'
+
+/**
+ * This configuration is used to for the Sanity Studio that’s mounted on the `/app/studio/[[...tool]]/page.tsx` route
+ */
+
+import {visionTool} from '@sanity/vision'
+import {defineConfig} from 'sanity'
+import {structureTool} from 'sanity/structure'
+import { deskTool } from "sanity/desk";
+import { schemaTypes } from "./schemas";
+import { codeInput } from "@sanity/code-input";
+import { table } from "@sanity/table";
+
+// Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
+import {apiVersion, dataset, projectId} from './sanity/env'
+import {schema} from './sanity/schemaTypes'
+import {structure} from './sanity/structure'
+
+export default defineConfig({
+  name: "Riaz",
+  title: "riaznur.com",
+  basePath: '/studio',
+  projectId,
+  dataset,
+  // Add and edit the content schema in the './sanity/schemaTypes' folder
+  schema: { types: schemaTypes },
+  plugins: [
+    structureTool({structure}),
+    deskTool(), 
+    codeInput(), 
+    table(),
+    // Vision is for querying with GROQ from inside the Studio
+    // https://www.sanity.io/docs/the-vision-plugin
+    visionTool({defaultApiVersion: apiVersion}),
+  ],
+})
